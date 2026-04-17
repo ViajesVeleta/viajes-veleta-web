39acc8fd57048257d761277b8e1ff6384e2f10c0
Javier Segovia Córdoba <7463564+JavierSegoviaCordoba@users.noreply.github.com>
2026-04-17 16:45:28 +0200

    feat(contact): redesign layout, update social links and SEO fixes (#38)

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Viajes Veleta';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Agencia de viajes en Guadix especializada en viajes en grupo, vacaciones a medida, circuitos culturales y escapadas inolvidables. Descubre tu próxima aventura con Viajes Veleta.';
